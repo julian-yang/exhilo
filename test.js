bd0b90910c73ffbb943ba69b649d0631340a7b6f
Julian Yang <yang.julian@gmail.com>
2016-11-12 18:14:29 -0800

    fix jquery selection for icons

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 */
 
 $(window).scroll(function () {
-  var bookIcon = $('#block-efb1dda1a2fa68cb2bd7');
-  var pencilIcon = $('#block-e6bf357e466457afaef7');
-  var handIcon = $('#block-83d83ebf35bf627b1d6b');
-  var icons = [bookIcon, pencilIcon, handIcon];
+  var bookIcon = '#block-efb1dda1a2fa68cb2bd7';
+  var pencilIcon = '#block-e6bf357e466457afaef7';
+  var handIcon = '#block-83d83ebf35bf627b1d6b';
+  var icons = $([bookIcon, pencilIcon, handIcon].join(', '));
   icons.each(function () {
     var topOfWindow = $(window).scrollTop(),
         bottomOfWindow = topOfWindow + $(window).height();
